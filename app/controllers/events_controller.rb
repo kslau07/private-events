578f80465132dfe1157d23a351d6a603787e44ef
sam <abluecar@gmail.com>
2023-06-07 01:23:05 -0400

    Style @events, links. Add helper method.
    Learned how to use helper methods.
    Created a method to add ordinal numbers.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,10 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
+  def index
+    @events = Event.all
+  end
+
   def new
     @current_user = User.find(current_user.id)
     @event = @current_user.events.build
@@ -19,10 +23,25 @@ class EventsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+  
+  helper_method :ordinal_number
 
   private
 
   def event_params
     params.require(:event).permit(:event_date, :description)
+  end
+
+  def ordinal_number(num)
+    case num
+    when '1'
+      "#{num}st,"
+    when '2'
+      "#{num}nd,"
+    when '3'
+      "#{num}rd,"
+    else
+      "#{num}th,"
+    end
   end
 end
