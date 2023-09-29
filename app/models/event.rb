6d8e2fb4416a47392125a62ad79c026cc8d45bb6
sam <abluecar@gmail.com>
2023-09-28 22:53:30 -0400

    Complete system/events/create testing
    Add another test in system/events/create_spec

--- app/models/event.rb
+++ app/models/event.rb
@@ -16,9 +16,10 @@ class Event < ApplicationRecord
   has_many :attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :attendances
 
+  validates :title, presence: true
+  validates :location, presence: true
   validates :event_date, presence: true
   validates :description, presence: true
-  validates :location, presence: true
 
   scope :past, -> { where('event_date < ?', Time.now) }
   scope :future, -> { where('event_date > ?', Time.now) }
--- spec/system/events/create_spec.rb
+++ spec/system/events/create_spec.rb
@@ -3,13 +3,13 @@
 require 'rails_helper'
 
 RSpec.feature 'Creating an event', type: :system do
-  feature 'foo' do
+  feature 'User creates an event with valid inputs' do
     before :each do
       user = FactoryBot.create(:user)
       login_as(user, scope: :user)
     end
 
-    scenario 'foo' do
+    scenario 'Event.count goes up by one after event is created' do
       visit root_path
       click_link 'add an event'
       fill_in 'event_title', with: 'My awesome event'
@@ -17,9 +17,41 @@ RSpec.feature 'Creating an event', type: :system do
       # HACK: couldn't use normal methods to find date
       find('input[type=date]').send_keys(:arrow_up, :tab, :arrow_up, :tab, :arrow_up)
       fill_in 'event_description', with: 'Expect fun times.'
+      initial_count = Event.count
       click_button 'create event'
-      sleep 5
-      expect(page).to have_content('foo')
+      sleep 1 # HACK: test fails otherwise
+      final_count = Event.count
+      difference = 1
+      expect(final_count - initial_count).to eq(difference)
+    end
+  end
+
+  feature 'User fills out event form incompletely and generates errors' do
+    before :each do
+      user = FactoryBot.create(:user)
+      login_as(user, scope: :user)
+    end
+
+    scenario 'When title is left blank user receives warning about blank title' do
+      visit root_path
+      click_link 'add an event'
+      fill_in 'event_title', with: ''
+      fill_in 'event_location', with: 'Bob\'s house'
+      find('input[type=date]').send_keys(:arrow_up, :tab, :arrow_up, :tab, :arrow_up)
+      fill_in 'event_description', with: 'This will be a cool event because...'
+      click_button 'create event'
+      expect(page).to have_text('Title can\'t be blank')
+    end
+
+    scenario 'When date is left blank user receives warning about blank date' do
+      visit root_path
+      click_link 'add an event'
+      fill_in 'event_title', with: 'Another rad event'
+      fill_in 'event_location', with: 'Joe\'s backyard'
+      # Do not fill out date
+      fill_in 'event_description', with: 'Joe\'s having a bbq'
+      click_button 'create event'
+      expect(page).to have_text('Event date can\'t be blank')
     end
   end
 end
