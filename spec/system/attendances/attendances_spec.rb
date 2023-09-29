07b8a481e95a2135de83cdd0ae8b4f6656ea592e
sam <abluecar@gmail.com>
2023-09-29 18:00:09 -0400

    Use anchor tag and #id for 'invite people' link

--- spec/system/attendances/attendances_spec.rb
+++ spec/system/attendances/attendances_spec.rb
@@ -1,9 +1,33 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe 'Attendances', type: :system do
+RSpec.feature 'Create an attendance (invitation)' do
   before do
-    driven_by(:rack_test)
+    visit '/'          # NOTE: delete me
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'change me' do
+    # NOTE: delete me -> let is available in top context, or in 'context' blocks
+    # NOTE: delete me -> 'before' blocks are available in top context, or in 'context' blocks
+    # NOTE: delete me -> let and before are the same in that regard
+    # Use this example in our testing
+    # within(".content-container") do
+    #   expect(page).to have_content("Example Name")
+    # end
+    # Can we incorporate this?
+    # subject(:flight_generator) { described_class.new(tomorrow) }
+
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:event) { FactoryBot.create(:event, creator: user) }
+
+    before do
+      visit '/'
+    end
+
+    # Create attendance
+    scenario 'pass in collaborator to factorybot' do
+      expect(event).to eq 1
+    end
+  end
 end
